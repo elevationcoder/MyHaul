28944c109c9aa6894297a02960f0358e6a15eae2
cosmicChimp <ian.t.marrs@gmail.com>
2020-04-16 10:27:00 -0600

    added index with values

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :offers
-  # resources :users
-  # resources :sessions
-  # resources :welcome
   resources :drivers
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
