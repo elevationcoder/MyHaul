abfa0c4d37f94c0de34fc2b1c08da2ee8ec201f7
cosmicChimp <ian.t.marrs@gmail.com>
2020-04-20 09:48:17 -0600

    minor changes

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,7 @@ class Offer < ApplicationRecord
     belongs_to :user
     belongs_to :driver, optional: true
     scope :open, -> { where(driver_id: nil) }
+    scope :contracted, -> { where(driver_id: !nil)}
     scope :above, ->(price) { where("price_offer > ?", price )}
 
     # scope :accepted_by, ->(driver) { }
