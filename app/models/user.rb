4ec4c65eb2059a2a7c7fe6634e1768567c9ddcb0
cosmicChimp <ian.t.marrs@gmail.com>
2020-04-30 12:59:22 -0600

    More finishing touches to views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
     has_many :offers
     has_many :drivers, through: :offers
     validates :password, presence: true
-    validates :email, presence: true
+    validates :email, presence: true, uniqueness: true
     has_secure_password
 end
